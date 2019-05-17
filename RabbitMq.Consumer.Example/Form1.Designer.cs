3fa93267d523468f27391c473cd836e702891c36
Eliaquim Mauricio <emauricio@tasken.com>
2019-05-17 10:54:29 -0300

    Primeiro commit.

---
+++ RabbitMq.Consumer.Example/Form1.Designer.cs
@@ -0,0 +1,145 @@
+﻿namespace RabbitMq.Consumer.Example
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtReceveidText = new System.Windows.Forms.TextBox();
+            this.btnConect = new System.Windows.Forms.Button();
+            this.txtServer = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtQueueName = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtId = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // txtReceveidText
+            // 
+            this.txtReceveidText.Location = new System.Drawing.Point(12, 172);
+            this.txtReceveidText.Multiline = true;
+            this.txtReceveidText.Name = "txtReceveidText";
+            this.txtReceveidText.ReadOnly = true;
+            this.txtReceveidText.ScrollBars = System.Windows.Forms.ScrollBars.Both;
+            this.txtReceveidText.Size = new System.Drawing.Size(482, 193);
+            this.txtReceveidText.TabIndex = 18;
+            // 
+            // btnConect
+            // 
+            this.btnConect.Location = new System.Drawing.Point(418, 143);
+            this.btnConect.Name = "btnConect";
+            this.btnConect.Size = new System.Drawing.Size(75, 23);
+            this.btnConect.TabIndex = 13;
+            this.btnConect.Text = "Conectar";
+            this.btnConect.UseVisualStyleBackColor = true;
+            this.btnConect.Click += new System.EventHandler(this.btnConect_Click);
+            // 
+            // txtServer
+            // 
+            this.txtServer.Location = new System.Drawing.Point(12, 73);
+            this.txtServer.Name = "txtServer";
+            this.txtServer.Size = new System.Drawing.Size(482, 20);
+            this.txtServer.TabIndex = 10;
+            this.txtServer.Text = "localhost";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(9, 57);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(115, 13);
+            this.label2.TabIndex = 14;
+            this.label2.Text = "Servidor do RabbitMQ:";
+            // 
+            // txtQueueName
+            // 
+            this.txtQueueName.Location = new System.Drawing.Point(12, 117);
+            this.txtQueueName.Name = "txtQueueName";
+            this.txtQueueName.Size = new System.Drawing.Size(482, 20);
+            this.txtQueueName.TabIndex = 12;
+            this.txtQueueName.Text = "rpc_queue";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(9, 101);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(120, 13);
+            this.label1.TabIndex = 11;
+            this.label1.Text = "Nome da fila de retorno:";
+            // 
+            // txtId
+            // 
+            this.txtId.Location = new System.Drawing.Point(12, 25);
+            this.txtId.Name = "txtId";
+            this.txtId.ReadOnly = true;
+            this.txtId.Size = new System.Drawing.Size(482, 20);
+            this.txtId.TabIndex = 19;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(9, 9);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(110, 13);
+            this.label3.TabIndex = 20;
+            this.label3.Text = "Meu ID de Consumer:";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(505, 381);
+            this.Controls.Add(this.txtId);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.txtReceveidText);
+            this.Controls.Add(this.btnConect);
+            this.Controls.Add(this.txtServer);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txtQueueName);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "RabbitMQ Consumer Example";
+            this.FormClosed += new System.Windows.Forms.FormClosedEventHandler(this.Form1_FormClosed);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtReceveidText;
+        private System.Windows.Forms.Button btnConect;
+        private System.Windows.Forms.TextBox txtServer;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtQueueName;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtId;
+        private System.Windows.Forms.Label label3;
+    }
+}
+
---
+++ RabbitMq.Consumer.Example/Form1.cs
@@ -0,0 +1,95 @@
+﻿using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System;
+using System.Text;
+using System.Threading;
+using System.Windows.Forms;
+
+namespace RabbitMq.Consumer.Example
+{
+    public partial class Form1 : Form, IDisposable
+    {
+        private IModel _channel;
+        
+        private readonly string _id = new Random().Next(100, 999).ToString();
+
+        delegate void ReceveidMessageHandler(string msg);
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            txtId.Text = _id;
+        }
+
+        private void btnConect_Click(object sender, EventArgs e)
+        {
+            string queue = txtQueueName.Text;
+
+            //Criando o canal de comunicação com o RabbitMQ.
+
+            _channel = new ConnectionFactory { HostName = txtServer.Text }.CreateConnection()
+                                                                          .CreateModel();
+
+            //Declarando a fila que será utilizada, caso não exista com esse nome ele irá criar automaticamente.
+
+            _channel.QueueDeclare(queue, false, false, true, null);
+
+            //Declarando um consumidor para rastrear todas as mensagem recebidas na fila.
+
+            EventingBasicConsumer consumer = new EventingBasicConsumer(_channel);
+            consumer.Received += ReceivedText;
+
+            _channel.BasicConsume(consumer, queue, true);
+
+            btnConect.Enabled = false;
+        }
+
+        private void ReceivedText(object model, BasicDeliverEventArgs ea)
+        {
+            string message = Encoding.UTF8.GetString(ea.Body);
+
+            //Escrevendo a mensagem recebida na tela.
+
+            AddMessage(message);
+
+            //Criando delay ficticio para testar
+
+            Thread.Sleep(10000);
+
+            //Crio as propriedades de resposta e associo o id de correlação recebido com o que será enviado.
+
+            IBasicProperties replyProps = _channel.CreateBasicProperties();
+            replyProps.CorrelationId = ea.BasicProperties.CorrelationId;
+
+            //Declarando a mensagem de resposta e enviando na mesma fila.
+
+            byte[] responseBytes = Encoding.UTF8.GetBytes($"O consumer [{_id}] recebeu e processou sua mensagem enviada: \"{message}\"");
+
+            _channel.BasicPublish(string.Empty, ea.BasicProperties.ReplyTo, replyProps, responseBytes);
+        }
+
+        private void AddMessage(string msg)
+        {
+            if (InvokeRequired)
+            {
+                ReceveidMessageHandler callback = AddMessage;
+                Invoke(callback, msg);
+                return;
+            }
+
+            txtReceveidText.Text += ">>> " + msg + Environment.NewLine;
+        }
+
+        private void Dispose()
+        {
+            _channel.Dispose();
+            GC.SuppressFinalize(this);
+        }
+
+        private void Form1_FormClosed(object sender, FormClosedEventArgs e)
+        {
+            Application.Exit();
+        }
+    }
+}
